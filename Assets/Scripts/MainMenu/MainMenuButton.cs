1bad22b0d8ae167cedf49951c25cc9d87e1de026
Armikus <armikusrpg@gmail.com>
2021-01-04 01:34:30 +0100

    Adding basic main menu

---
+++ Assets/Scripts/MainMenu/MainMenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void startNewGame() {
+        Debug.Log("Starting New Game");
+    }
+
+    public void continueGame() {
+        Debug.Log("Continue existing game");
+    }
+
+    public void lanuchExampleFight() {
+        Application.LoadLevel(1);
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+}
