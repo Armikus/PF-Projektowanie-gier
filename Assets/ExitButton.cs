840bb73d793420199f0bfc0801c7067d56472b5e
Armikus <armikusrpg@gmail.com>
2021-01-31 01:28:07 +0100

    Bug fixes, defeat/winning screen

---
+++ Assets/ExitButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    void start() { }
+
+    void update() { }
+
+
+    public void exitOnClick() {
+        Debug.Log("Coming Back to main menu");
+        Application.LoadLevel(0);
+    }
+}
